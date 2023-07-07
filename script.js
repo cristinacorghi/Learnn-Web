a183d94006f1d9e7f61b4b02fd79ea161509c742
CristinaCorghiAPVD <cristina.c@apvd.it>
2023-07-07 12:58:31 +0200

    back to top

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// const btn = document.getElementById('button');
-// btn.addEventListener('click', function() {
-//     console.log('Click avvenuto');
-// });
+// Back to top
+const backBtn = document.getElementById('backBtn');
+
+backBtn.addEventListener('click', function() {
+    window.scrollTo(0, 0);
+});
+
+window.addEventListener('scroll', function() {
+    let posY = this.window.scrollY;
+    
+    if (posY > 0) {
+        backBtn.style.display = 'block';
+    } else {
+        backBtn.style.display = 'none';
+    }
+});
